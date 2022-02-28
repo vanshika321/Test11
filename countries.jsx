a9d7bd1e7b3c8a389658e36db966548e2c97ca51
vanshika321 <67959174+vanshika321@users.noreply.github.com>
2022-02-28 19:14:31 +0530

    Add files via upload

---
+++ countries.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import{getData} from "./data"; 
+
+class Countries extends Component {
+    state = {
+        data   : getData(),
+        searchTerm:""
+
+      } 
+    render() { 
+        return (
+            <div>
+                 <input type="text"placeholder="Search" 
+        onChange={(e)=>{
+          this.setState({searchTerm:e.target.value})
+        }}/>
+                {this.state.data.filter((val)=>{
+            if(this.state.searchTerm==="")
+                return val
+                else if(val.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+                    return val
+          }).map((obj) => (
+           <table>
+               <tr>Country Name</tr>
+               <td>{obj.country}</td>
+               <tr> Name</tr>
+               <td>{obj.name}</td>
+           </table>
+          ))}
+                
+            </div>
+        );
+    }
+}
+ 
+export default Countries;
---
+++ data.js
@@ -0,0 +1,55 @@
+const data=[{
+    
+    name:"test1",
+    email:"testtemail.com",
+    address:"testad",
+    phone:123,
+    country:"Country"
+
+
+},
+{
+
+name:"test2",
+email:"testtemail.com",
+address:"testad",
+phone:123,
+country:"India"
+
+
+},
+{
+
+name:"test3",
+email:"testtemail.com",
+address:"testad",
+phone:123,
+country:"Australlia"
+
+
+},
+{
+
+name:"test4",
+email:"testtemail.com",
+address:"testad",
+phone:123,
+country:"USA"
+
+
+},{
+
+name:"test5",
+email:"testtemail.com",
+address:"testad",
+phone:123,
+country:"China"
+
+
+},
+
+
+]
+export function getData(){
+    return data
+};
